7d9d60c765ff4f2cac1ea99254326568e62459cd
ngdong <2559635030@qq.com>
2018-02-10 16:14:45 +0800

    sd

--- src/assets/picker.js
+++ src/assets/picker.js
@@ -1,10 +1,13 @@
 (function () {
   let that = this;
 
-  let isSayingFlag = false;
-
   function init() {
-
+    // 创建Picker选取DOM
+    if (!document.getElementById('pickerSelectWrap')) {
+      var pickerSelectWrap = document.createElement('div');
+      pickerSelectWrap.id = 'pickerSelectWrap';
+      document.body.appendChild(pickerSelectWrap);
+    }
   }
 
   function show() {
